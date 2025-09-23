468ab0a646c5273df5e24d15811fbebec0a41c55
把爱留在798 <CPX5210375@gmail.com>
2025-09-23 15:54:33 +0800

    Icon resource.h

--- Physics Engine/resource.h
+++ Physics Engine/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ Éú³ÉµÄ°üº¬ÎÄ¼ş¡£
-// ¹© Physics Engine.rc Ê¹ÓÃ
+// Microsoft Visual C++ ç”Ÿæˆçš„åŒ…å«æ–‡ä»¶ã€‚
+// ä¾› Physics Engine.rc ä½¿ç”¨
 //
 #define IDI_ICON1                       101
 
